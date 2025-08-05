2696797d956c7bab3908a8e66232a6a4f25cbbaa
Geming400 <Geming400@gmail.com>
2025-08-05 22:07:59 +0200

    removed very useful log

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,6 @@ class $modify(PlayerObjectButBetter, PlayerObject) {
     void playerDestroyed(bool p0) {
         PlayerObject::playerDestroyed(true);
 
-        log::info("Played died: {}", p0);
-
         // Testing this amazing mod
         // ScrewYou2Manager::get()->killClass("MenuLayer");
         for (size_t i = 0; i < Mod::get()->getSettingValue<int64_t>("intensity"); i++) {
