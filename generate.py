e87e66e63e39ef8a1ad5b144b16d35f71b6048a4
Geming400 <Geming400@gmail.com>
2025-08-05 21:47:13 +0200

    maybe ?

--- generate.py
+++ generate.py
@@ -280,7 +280,7 @@ bool getReturnValue() {
 #define SCREWYOU2_MENULAYER_CUSTOM_INIT(className, ...) { \\
         log::info("Class {} has been killed: {}", CLASS_NAME, ScrewYou2Manager::get()->isKilled(CLASS_NAME)); \\
         if (ScrewYou2Manager::get()->isKilled(CLASS_NAME) && Mod::get()->getSettingValue<bool>("enabled")) return getReturnValue(); \\
-        if (!##className::init(__VA_ARGS__)) return false; \\
+        if (!className::init(__VA_ARGS__)) return false; \\
         if (Mod::get()->getSavedValue<bool>("first-time-loading", true)) { \\
             log::info("Showing popup"); \\
             Mod::get()->setSavedValue<bool>("first-time-loading", false); \\
@@ -303,7 +303,7 @@ class $modify(Screwd##className, className) { \\
 #define SCREWYOU2_HOOK_INIT(className, ...) { \\
         log::info("Class {} has been killed: {}", CLASS_NAME, ScrewYou2Manager::get()->isKilled(CLASS_NAME)); \\
         if (ScrewYou2Manager::get()->isKilled(CLASS_NAME) && Mod::get()->getSettingValue<bool>("enabled")) return getReturnValue();\\
-        if (!##className::init(__VA_ARGS__)) return false; \\
+        if (!className::init(__VA_ARGS__)) return false; \\
         return true; \\
     } \\
 };
